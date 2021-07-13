9027ead8e36be4cea7a026eadf2763fbdaa6da1c
melamito <gaby.castro@outlook.com>
2021-07-13 09:54:00 -0400

    Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { BooksComponent } from './components/books/books.component';
+import { QuejasComponent } from './components/quejas/quejas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    BooksComponent,
+    QuejasComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/quejas/quejas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quejas',
+  templateUrl: './quejas.component.html',
+  styleUrls: ['./quejas.component.scss']
+})
+export class QuejasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
